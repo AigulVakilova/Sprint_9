5ec95a7134fc11dfe7daf2286c89427033014ce7
AigulVakilova <vakilowaa@yandex.ru>
2026-05-29 17:33:20 +0400

    fix locators

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
-    TITLE = (By.CSS_SELECTOR, "h1.styles_title__2fhty")
+    TITLE = (By.XPATH, "//h1[text()='Войти на сайт']")
     FORM = (By.CSS_SELECTOR, "form.styles_form__2nwxz")
     EMAIL_INPUT = (By.NAME, "email")
     PASSWORD_INPUT = (By.NAME, "password")
-    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button.style_button__1FFWl")
+    SUBMIT_BUTTON = (By.XPATH, "//button[text()='Войти']")
     
--- locators/registration_page_locators.py
+++ locators/registration_page_locators.py
@@ -1,12 +1,12 @@
 from selenium.webdriver.common.by import By
 
 class RegistrationPageLocators:
-    TITLE = (By.CSS_SELECTOR, "h1.styles_title__2fhty")
+    TITLE = (By.XPATH, "//h1[text()='Регистрация']")
     FORM = (By.CSS_SELECTOR, "form.styles_form__2nwxz")
     FIRST_NAME_INPUT = (By.NAME, "first_name")
     LAST_NAME_INPUT = (By.NAME, "last_name")
     USERNAME_INPUT = (By.NAME, "username")
     EMAIL_INPUT = (By.NAME, "email")
     PASSWORD_INPUT = (By.NAME, "password")
-    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button.style_button__1FFWl")
+    SUBMIT_BUTTON = (By.XPATH, "//button[text()='Создать аккаунт']")
     
